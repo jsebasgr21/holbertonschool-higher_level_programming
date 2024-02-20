7c404eb249a4a26da0e57c4e43bb61d98540e42e
jsebasgr21 <jsebasgr21@hotmail.com>
2024-02-19 19:55:30 -0500

    testing code fix

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     cpylist = list.copy(my_list)
-    if idx < 0 or idx > (len(my_list)):
+    if idx < 0 or idx >= (len(my_list)):
         return cpylist
 
     for i in range(len(my_list)):
