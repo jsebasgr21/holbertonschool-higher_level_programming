6d52b95af32254500c3c83c96bef37fe2a6f0cc3
jsebasgr21 <jsebasgr21@hotmail.com>
2024-05-23 18:58:40 -0500

    0-safe_print_list.py

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+
+        count = 0
+        while count < x:
+            print(my_list[count], end="")
+            count += 1
+
+    except IndexError:
+        pass
+
+    print("")
+
+    return count
