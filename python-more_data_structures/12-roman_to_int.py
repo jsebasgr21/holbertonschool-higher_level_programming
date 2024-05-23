b95fdc349be2c8a07af31e15de5b67159cd3b9ce
jsebasgr21 <jsebasgr21@hotmail.com>
2024-05-23 13:59:17 -0500

    better index

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,30 +1,16 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    roman_value = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-
+    roman_value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+                   'M': 1000}
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-
     result = 0
-
     for i in range(len(roman_string)):
         value = roman_value[roman_string[i]]
-
         if (i < len(roman_string) - 1 and
                 roman_value.get(roman_string[i + 1]) > value):
-
             result -= value
-
         else:
             result += value
-
     return result
