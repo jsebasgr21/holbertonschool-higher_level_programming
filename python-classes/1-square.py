323f846f997b3dd9eca77709081804e83b220301
jsebasgr21 <jsebasgr21@hotmail.com>
2024-06-02 20:34:39 -0500

    add documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
+"""
+This module defines a class Square.
+"""
+
+
 class Square:
+    """
+    A class that defines a square by its size.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
     def __init__(self, size):
+        """
+        Initializes the Square with a given size.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
