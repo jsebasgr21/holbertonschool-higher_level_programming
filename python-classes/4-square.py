78fe735501bdf619d8e294ca3e7f29a3f9d65485
jsebasgr21 <jsebasgr21@hotmail.com>
2024-06-03 00:29:57 -0500

    hot fix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -18,12 +18,8 @@ class Square:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = int(size)
+
+        self.size = size
 
     def area(self):
         """
