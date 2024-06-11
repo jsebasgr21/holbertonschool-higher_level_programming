c017a5ac4add1adaa331571b92c40ffabbb8a3b5
jsebasgr21 <jsebasgr21@hotmail.com>
2024-06-11 01:05:44 -0500

    fix white trallingspace

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -3,8 +3,8 @@ class Rectangle:
     """
     Rectangle class that represents a rectangle.
 
-    This class currently has no attributes or methods. 
-    In the future, attributes like width and height can be added, 
+    This class currently has no attributes or methods.
+    In the future, attributes like width and height can be added,
     as well as methods to calculate the area and perimeter.
 
     Methods:
