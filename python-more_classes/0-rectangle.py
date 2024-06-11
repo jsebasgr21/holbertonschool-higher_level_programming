e9e3064e43311ed5dadc54573699d30a68c587b8
jsebasgr21 <jsebasgr21@hotmail.com>
2024-06-11 01:03:03 -0500

    add documentation

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 class Rectangle:
+    """
+    Rectangle class that represents a rectangle.
+
+    This class currently has no attributes or methods. 
+    In the future, attributes like width and height can be added, 
+    as well as methods to calculate the area and perimeter.
+
+    Methods:
+    --------
+    None defined in this implementation.
+    """
     pass
