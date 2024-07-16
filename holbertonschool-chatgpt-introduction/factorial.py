7bd932233268c5c7ff7e7ecdb6af8d2da57fe66c
jsebasgr21 <jsebasgr21@hotmail.com>
2024-07-15 20:22:56 -0500

    testing

--- holbertonschool-chatgpt-introduction/factorial.py
+++ holbertonschool-chatgpt-introduction/factorial.py
@@ -10,12 +10,5 @@ def factorial(n):
     return result
 
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        try:
-            f = factorial(int(sys.argv[1]))
-            print(f)
-        except ValueError:
-            print("Please provide a valid integer as an argument.")
-    else:
-        print("Please provide an integer as an argument.")
+f = factorial(int(sys.argv[1]))
+print(f)
