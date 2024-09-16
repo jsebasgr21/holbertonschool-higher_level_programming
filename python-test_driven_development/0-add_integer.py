440e98eee6a0543fe7349c4f1f2f94355537bdd4
jsebasgr21 <jsebasgr21@hotmail.com>
2024-09-15 22:39:33 -0500

    module documented

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+""" this module contain a function that adds two numbers integers
+and/or float numbers"""
+
+
 def add_integer(a, b=98):
-    """ Function that adds two integer and/or float numbers """
+    """Function that adds two integer and/or float numbers"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
