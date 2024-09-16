9b0fa2372e5c19617651d4f663d3f1e08c3c6a66
jsebasgr21 <jsebasgr21@hotmail.com>
2024-09-16 00:20:15 -0500

    test 4 pass matrix must be a list of lists of integers/floats

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,18 +8,19 @@ def matrix_divided(matrix, div):
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
     if not isinstance(matrix, list):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if not all(isinstance(num, list) for num in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for row in matrix:
+        if not all(isinstance(num, (int, float)) for num in row):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
 
     row_lenght = len(matrix[0])
     for row in matrix:
         if len(row) != row_lenght:
             raise TypeError("Each row of the matrix must have the same size")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
 
     return [[round(num / div, 2) for num in row] for row in matrix]
