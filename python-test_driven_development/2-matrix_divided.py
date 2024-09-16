1498e8c0a775ef9cec9fec898e42e2d127dcf421
jsebasgr21 <jsebasgr21@hotmail.com>
2024-09-16 00:01:29 -0500

    testing pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ a function that divides all elements of a matrix. """
 
+
 def matrix_divided(matrix, div):
-    """ function that divides the integer or float numbers
+    """function that divides the integer or float numbers
     of a matrix"""
 
-    if not isinstance (div, (int, float)):
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
@@ -21,4 +22,4 @@ def matrix_divided(matrix, div):
         if len(row) != row_lenght:
             raise TypeError("Each row of the matrix must have the same size")
 
-    return[[round(num/div, 2) for num in row] for row in matrix]
+    return [[round(num / div, 2) for num in row] for row in matrix]
