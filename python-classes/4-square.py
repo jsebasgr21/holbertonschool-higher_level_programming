f87834b1abcc88efcefee22c91596d726552b42f
jsebasgr21 <jsebasgr21@hotmail.com>
2024-09-22 16:27:24 -0500

    used black

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -28,7 +28,7 @@ class Square:
         Returns:
             int: The area of the square.
         """
-        return (self.__size ** 2)
+        return self.__size ** 2
 
     @property
     def size(self):
